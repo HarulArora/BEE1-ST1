3457bffadaea04fae1481ce9b5b9e7145f32d901
HarulArora <81470702+HarulArora@users.noreply.github.com>
2023-11-30 20:29:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://harul:BdbYOHafzwEkI0hY@joyboy.sc2cbyz.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
+
